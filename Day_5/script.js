ddfaad38ac9371bfd3a62856c7b0db555563f19f
Aaryan Tiwari <153616544+BlueMoonerATcode@users.noreply.github.com>
2024-01-03 22:36:52 +0530

    Update script.js

--- Day_5/script.js
+++ Day_5/script.js
@@ -1,3 +1,13 @@
-const arrowmul = (a,b) => {
-    console.log(a*b);
+const arrowmul = (a, b) => {
+    console.log(a * b);
 };
+
+function countVowels(str) {
+    let count = 0;
+    for (const char of str) {
+        if (char === "a" || char === "e" || char === "i" || char === "o" || char == "u") { count++; }
+    }
+    console.log(count);
+}
+
+countVowels("aaryan");
