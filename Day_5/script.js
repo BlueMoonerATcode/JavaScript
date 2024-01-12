f72a192f9772a5eaace8470012dba9b45e425925
Aaryan Tiwari <153616544+BlueMoonerATcode@users.noreply.github.com>
2024-01-12 15:11:49 +0530

    Update script.js

--- Day_5/script.js
+++ Day_5/script.js
@@ -1,7 +1,9 @@
+//Arrow Function
 const arrowmul = (a, b) => {
-    console.log(a * b);
+    return a * b;
 };
 
+//Function fot Counting Vowels
 function countVowels(str) {
     let count = 0;
     for (const char of str) {
@@ -10,4 +12,41 @@ function countVowels(str) {
     console.log(count);
 }
 
-countVowels("aaryantiwari");
+console.log("No. of Vowels are: ");
+countVowels("aaryan"); //Calling Counting Vowels function
+
+arrowmul(5, 7) //Calling Arrow Function
+
+//Finding Square for Numbers using forEach
+console.log("Squares of the numbers 1,2,3,4,5 are: ");
+let arr = [1, 2, 3, 4, 5]
+arr.forEach(function printVal(val) {
+    console.log(val * val);
+})
+
+//Map Method
+let nums = [12, 21, 13, 31];
+let newArr = nums.map((val) => { return val; });
+console.log(newArr);
+
+//Filter Method
+let brr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+let evenArr = brr.filter((val) => { return val % 2 === 0; });
+console.log(evenArr);
+
+//Reduce Method
+const output = brr.reduce((res, curr) => { return res + curr; })
+console.log(output);
+
+//Filter for marks (90+)
+let marks = [21, 43, 6, 46, 89, 96, 95, 46, 90, 35, 75, 86, 89]
+const result = marks.filter((std) => { return std >= 90; })
+console.log(result)
+
+//Enter a number for printing array till that n
+let n = prompt("Enter your No.: ");
+let crr = [];
+for (let i = 1; i <= n; i++) {
+    crr[i - 1] = i;
+}
+console.log(crr)
