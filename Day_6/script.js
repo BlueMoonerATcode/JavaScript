10e4d332414b2e522211bf5919fae498fe5297a4
Aaryan Tiwari <153616544+BlueMoonerATcode@users.noreply.github.com>
2024-01-13 10:35:15 +0530

    Update script.js

--- Day_6/script.js
+++ Day_6/script.js
@@ -1,1 +1,18 @@
 alert("Hello!")
+
+let allEl = document.querySelectorAll("p");
+console.dir(allEl);
+
+//Access h3 element using tag
+let h3 = document.querySelector("h3")
+console.dir(h3.innerText); //To change: Using inner text property
+h3.innerText = h3.innerText + " Lets Learn!"; //Concatenate
+
+
+//For of Loop for modifying Boxs (Divs)
+let divs = document.querySelectorAll(".box")
+let idx = 1;
+for (div of divs) {
+    div.innerText = `Updated Value ${idx}`;
+    idx++;
+}
